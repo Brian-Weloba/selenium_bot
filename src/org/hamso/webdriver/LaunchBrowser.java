c2f30b2d162545c1f97d0dea571fcbf16e78cd2e
hamsoace <hamsoace@gmail.com>
2021-12-05 19:33:27 +0300

    could login to mymovies africa

--- src/org/hamso/webdriver/LaunchBrowser.java
+++ src/org/hamso/webdriver/LaunchBrowser.java
@@ -28,21 +28,56 @@ public class LaunchBrowser {
             System.out.println(title);
         }
 
-        // TODO: locate a web element
-        String tagname= " ";
-        tagname=driver.findElement(By.cssSelector("btn btn-nav btn-warning")).getText();
+        // TODO: login to mymovies africa
+        String tagname= "";
+        tagname=driver.findElement(By.cssSelector("a[class='btn btn-nav btn-warning']")).getText();
         System.out.println(tagname);
 
-        WebElement category = driver.findElement(By.cssSelector("btn btn-nav btn-warning"));
+        WebElement category = driver.findElement(By.cssSelector("a[class='btn btn-nav btn-warning']"));
         Actions action = new Actions(driver);
         action.moveToElement(category);
         action.click();
         action.perform();
         Thread.sleep(3000);
 
-        driver.findElement(By.linkText("LOGIN")).click();
-        Thread.sleep(2000);
+//        driver.findElement(By.linkText("LOGIN")).click();
+//        Thread.sleep(2000);
 
+        loginAccount(driver);
+    }
+    private static void loginAccount(WebDriver driver) throws InterruptedException {
+//        String loginname = "";
+//        loginname=driver.findElement(By.cssSelector("button[class='swal2-confirm swal2-styled']")).getText();
+//        System.out.println(loginname);
 
+        WebElement login = driver.findElement(By.cssSelector("button[class='swal2-confirm swal2-styled']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(login).click(login).build().perform();
+        Thread.sleep(3000);
+
+        WebElement email= driver.findElement(By.name("email"));
+        email.clear();
+        email.sendKeys("hamsoace@gmail.com");
+        System.out.println(email);
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.clear();
+        password.sendKeys("Sumeshu1");
+        System.out.println(password);
+
+        WebElement submit = driver.findElement(By.cssSelector("button[class='btn btn-lg btn-block btn-warning']"));
+        Actions actions1 = new Actions(driver);
+        actions1.moveToElement(submit).click(submit).build().perform();
+        Thread.sleep(3000);
+
+        WebElement phoneNumber = driver.findElement(By.cssSelector("button[class='swal2-confirm swal2-styled']"));
+        Actions actions2 = new Actions(driver);
+        actions1.moveToElement(phoneNumber).click(phoneNumber).build().perform();
+        Thread.sleep(3000);
+
+        watchMovie(driver);
+    }
+
+    private static void watchMovie(WebDriver driver) {
     }
 }
